b572b89b121fbb461564b8581ff73124e0b897dd
vigvega <victoriagv01@gmail.com>
2022-06-20 17:36:14 +0200

    Images and events

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function sombraTorre(){
+        document.getElementById("rojeando1").style.backgroundColor = "red";
+        document.getElementById("rojeando2").style.backgroundColor = "red";
+    }
